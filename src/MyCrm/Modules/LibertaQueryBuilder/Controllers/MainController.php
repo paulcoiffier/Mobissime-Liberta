7729b62e43e458f7ab680dfe3ccfddf747244e0b
Paul Coiffier <coiffier.paul@gmail.com>
2015-05-17 23:30:51 +0200

    Rework completly query builder module

--- src/MyCrm/Modules/LibertaQueryBuilder/Controllers/MainController.php
+++ src/MyCrm/Modules/LibertaQueryBuilder/Controllers/MainController.php
@@ -1,23 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Paul
- * Date: 28/01/2015
- * Time: 15:56
- */
 
 namespace MyCrm\Modules\LibertaQueryBuilder\Controllers;
 
 use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Request;
 use App\Lib\TwigController;
-use MyCrm\Modules\LibertaDatastore\Model\DataStoreModel;
-use App\Services\AnnotationsParser;
+use MyCrm\Modules\LibertaModules\Model\ModulesModel;
 
 class MainController extends TwigController
 {
 
-    public function indexAction(Request $request, $appParams)
+    public function indexAction(Request $request, $appParams, $moduleId)
     {
 
         /** Internal controller mixture */
@@ -25,68 +18,18 @@ class MainController extends TwigController
         $this->setRequest($request);
         /** End internal controller mixture */
 
-        $model = new DataStoreModel();
+        /** Read XML Module queries file "queries.xml" */
+        $moduleModel = new ModulesModel($appParams['entityManager']);
+        $moduleToView = $moduleModel->getModuleById($moduleId);
+        $queries = simplexml_load_file(install_sys_dir . 'src/MyCrm/Modules/' . $moduleToView->getModName() . '/Conf/queries.xml');
 
+        /** Apply page template and parameters */
         $this->setPageTemplate('Index.html');
-        $this->setModuleRendererOptions(array('entities' => $model->getEntitiesList()));
+        $this->setModuleRendererOptions(array('queries' => $queries));
+        // print_r($queries);
 
         /** Response */
         return $this->getResponse();
-    }
-
-    public function entityTableAction(Request $request, $appParams)
-    {
-        $request = Request::createFromGlobals();
-
-        if ($request->isXmlHttpRequest()) {
-            /** Internal controller mixture */
-            $this->setValues(__DIR__ . '/../Views/', $appParams, $appParams['container']);
-            $this->setRequest($request);
-            /** End internal controller mixture */
-
-            $parser = new AnnotationsParser();
-            $entity = $_REQUEST['entity'];
-            $entity_name = $_REQUEST['entity'];
-
-            $entity = "\App\Entities\\" . $entity;
-            $class = new $entity;
-            $classFields = $parser->getClassPropertiesList($class);
-
-            $r = new \ReflectionClass($class);
-            $doc = $r->getDocComment();
-            preg_match_all('#@(.*?)\n#s', $doc, $annotations);
-
-            $array_fields = array();
-            $i = 0;
-
-            foreach ($classFields as $field) {
-
-                $array = $parser->getPropertyAnnotations($class, $field);
-                $arrayB = array_shift($array);
-
-                foreach ($arrayB as $key => $value) {
-                    $array_fields[$i][$key] = $value;
-                }
-
-                $array_fields[$i]['object_type'] = get_class($arrayB);
-                $array_fields[$i]['display_type'] = "Text";
-                $array_fields[$i]['display_width'] = "";
-                $array_fields[$i]['display_tooltip'] = "";
-                $array_fields[$i]['display_error'] = "";
-                $array_fields[$i]['display_length'] = "";
-                $array_fields[$i]['display_mandatory'] = "";
-                $array_fields[$i]['display_mandatory_label'] = "";
-
-                $array_fields[$i]["name"] = $field;
-                $i++;
-            }
-
-            $this->setPageTemplate('EntityTable.html');
-            $this->setModuleRendererOptions(array('entity' => $entity_name, 'fields' => $array_fields));
-
-            /** Response */
-            return $this->getResponse();
-        }
     }
 
 }
---
+++ src/MyCrm/Modules/LibertaQueryBuilder/Model/QueriesModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MyCrm\Modules\LibertaModules\Lib;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Paul
+ * Date: 17/05/2015
+ * Time: 23:08
+ */
+
+class QueriesModel {
+
+    /** Get list of module queries
+     * @param $moduleName : Module Name
+     */
+    public function getQueries($moduleName){
+
+    }
+
+}
